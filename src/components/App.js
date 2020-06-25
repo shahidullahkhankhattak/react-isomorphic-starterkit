2bc905b63b9c1007a3091ae89ea36623df1e4fec
Adil Sikandar <zeeshanhurmat@apples-mbp-2.home>
2020-06-25 14:06:45 -0400

    refactored boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,18 @@ import PropTypes from 'prop-types';
 import StyleContext from 'isomorphic-style-loader/StyleContext';
 import { Provider as ReduxProvider } from 'react-redux';
 import ApplicationContext from './ApplicationContext';
+import '../assets/css/main.css';
 
-/**
- * The top-level React component setting context (global) variables
- * that can be accessed from all the child components.
- *
- * https://facebook.github.io/react/docs/context.html
- *
- * Usage example:
- *
- *   const context = {
- *     history: createBrowserHistory(),
- *     store: createStore(),
- *   };
- *
- *   ReactDOM.render(
- *     <App context={context} insertCss={() => {}}>
- *       <Layout>
- *         <LandingPage />
- *       </Layout>
- *     </App>,
- *     container,
- *   );
- */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+ //import 'popper.js/dist/popper';
+ //import 'bootstrap/dist/js/bootstrap.min';
+// import '../assets/js/main';
 
 export default function App({ context, insertCss, children }) {
   // NOTE: If you need to add or modify header, footer etc. of the app,
   // please do that inside the Layout component.
+
   return (
     <StyleContext.Provider value={{ insertCss }}>
       <ApplicationContext.Provider value={{ context }}>
